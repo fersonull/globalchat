4786d7c31c66adb0b28cd628e421e4d49d31f658
Casper <culaspero@gmail.com>
2025-04-30 19:25:36 +0800

    removed codes used for debugging.

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-use App\Classes\User;
 
-$user = new User();
-
-$res = $user->getAllUsers();
-
-// foreach ($res as $user) {
-//     echo $user['email'];
-// }
-
-$user1 = $user->getUser(1000);
-
-print_r($user1);
